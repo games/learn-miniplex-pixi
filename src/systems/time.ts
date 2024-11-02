11f20ccc0e95d5ff0f950b3f724d94f1ed3b614e
valor <valorzhong@gmail.com>
2024-11-02 11:27:05 +0800

    adjust game time logic

--- src/systems/time.ts
+++ src/systems/time.ts
@@ -4,24 +4,25 @@ import { Ticker } from 'pixi.js'
 import { GameTime } from '../views/GameTime'
 
 // 1 day in game is 1000ms
-const dayInGame = 1000
+const dayPerMs = 1000
 
 export function time(world: World<Entity>) {
     const times = world.with('time')
-    let lastUpdate = 0
     let totalDays = 0
+    let totalMS = 0
 
     return (ticker: Ticker) => {
-        if (ticker.lastTime - lastUpdate < dayInGame) {
+        totalMS += ticker.deltaMS
+        if (totalMS < dayPerMs) {
             return
         }
-        lastUpdate = ticker.lastTime
-        
+
         const [entity] = times
         const { time } = entity
 
         totalDays = time.year * 360 + time.month * 30 + time.day + 1
-        
+        totalMS -= dayPerMs
+
         time.year = Math.floor(totalDays / 360)
         time.month = Math.floor((totalDays % 360) / 30)
         time.day = Math.floor((totalDays % 360) % 30)
